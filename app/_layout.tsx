c4b9383613fc8c76a716151aee0cbbb0d8034f5a
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-09-12 20:45:40 +0530

    minor structure updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,9 +10,9 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal', headerShown: true }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
